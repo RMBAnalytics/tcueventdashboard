6543108e447d730a1f3f068c75573f57bd816a4d
RMBAnalytics <robsworld715@gmail.com>
2025-07-25 12:10:27 -0500

    Update TCUEventv2.py

--- TCUEventv2.py
+++ TCUEventv2.py
@@ -102,7 +102,7 @@ chapter_chart = alt.Chart(
     x=alt.X('Registrants:Q', title='Total Registrants'),
     y=alt.Y('Chapter/Club/Group:N', sort='-x', title='Chapter/Group', axis=alt.Axis(labelLimit=1000, labelAngle=0)),
     tooltip=['Chapter/Club/Group', 'Registrants']
-).properties(height=775)
+).properties(height=785)
 
 st.altair_chart(chapter_chart, use_container_width=True)
 
