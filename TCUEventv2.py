76844a6afb3f70ebbe02758d709a8520ac7d799e
RMBAnalytics <robsworld715@gmail.com>
2025-07-25 12:05:43 -0500

    Update TCUEventv2.py

--- TCUEventv2.py
+++ TCUEventv2.py
@@ -100,7 +100,7 @@ chapter_chart = alt.Chart(
     filtered_df.groupby('Chapter/Club/Group')['Registrants'].sum().reset_index()
 ).mark_bar(size=12).encode(
     x=alt.X('Registrants:Q', title='Total Registrants'),
-    y=alt.Y('Chapter/Club/Group:N', sort='-x', title='Chapter/Group', axis=alt.Axis(labelLimit=None)),
+    y=alt.Y('Chapter/Club/Group:N', sort='-x', title='Chapter/Group', axis=alt.Axis(labelLimit=1000)),
     tooltip=['Chapter/Club/Group', 'Registrants']
 ).properties(height=600)
 
